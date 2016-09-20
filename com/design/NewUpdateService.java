ee552c5169c300a75c0f1232ca54d9311bf45d1e
crazylearner <crazylearner@users.noreply.github.com>
2016-09-20 23:06:25 +0530

    Update NewUpdateService.java

--- com/design/NewUpdateService.java
+++ com/design/NewUpdateService.java
@@ -1,5 +1,13 @@
 package com.design;
 
+package com.design;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
 public interface NewUpdateService {
   void update(AssetUpdateProperties assetUpdateProperties, List<Object> dataToUpdate);
 }
