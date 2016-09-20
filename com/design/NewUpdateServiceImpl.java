fed84d4f5fbb050868b1d8c683dfc8544bc430a1
crazylearner <crazylearner@users.noreply.github.com>
2016-09-20 23:07:14 +0530

    Update NewUpdateServiceImpl.java

--- com/design/NewUpdateServiceImpl.java
+++ com/design/NewUpdateServiceImpl.java
@@ -1,5 +1,11 @@
 package com.design;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
 public class NewUpdateServiceImpl implements NewUpdateService{
 
   public void update(AssetUpdateProperties assetUpdateProperties, List<Object> dataToUpdate) {
